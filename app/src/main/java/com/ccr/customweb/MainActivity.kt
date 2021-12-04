0965f0170729bb0cec3def6a072ec95be9fe48e0
Achenglove <acheng625901>
2021-12-04 22:37:33 +0800

    add

--- app/src/main/java/com/ccr/customweb/MainActivity.kt
+++ app/src/main/java/com/ccr/customweb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ccr.customweb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.just.agentweb.AgentWeb
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
